d13c2d1f4c49582c9db42ca6fa2f40a6d3002da3
Vaithees-V <23lad07@gmail.com>
2026-09-10 15:31:07 +0530

    Added js

---
+++ script.js
@@ -0,0 +1,9 @@
+var sidenav = document.getElementById("sidenav")
+
+function showNavbar() {
+    sidenav.classList.remove("hidden")
+}
+
+function closeNavbar() {
+    sidenav.classList.add("hidden")
+}
